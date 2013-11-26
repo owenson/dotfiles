5c1c21147ae7947b920d86b196c60b417b966491
Gareth Owen <drgowen@gmail.com>
2013-11-26 11:50:19 +0000

    m

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-for f in *
+for f in vimrc vim conky zshrc
 do
    if [ $f == "install.sh" ]; then
    	continue
    fi
    CMD="ln -s `pwd`/$f ../.$f"
+   echo	$CMD
+   $CMD
+done
+
+for f in globals/*
+do
+   CMD="sudo ln -s `pwd`/$f /etc/$(basename $f)"
    echo	$CMD
    $CMD
 done
